e2d416c3f413889b2883c22dcf3bcf286451a205
aditigupta0 <aditi.gupta@truefoundry.com>
2024-10-04 19:07:24 +0530

    limit introduced 2

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def evaluate_model(model_name, task_name, batch_size, limit=None):
     run_name = re.sub(r'[^a-zA-Z0-9]', '-', model_name+"-"+task_name)
     bashCommand = "lm_eval --model hf --model_args pretrained={model_name},dtype=float --tasks {task_name} --device cuda:0 --batch_size {batch_size} --output_path ./results --log_samples --wandb_args project=lm-eval-harness-integration,name={run_name}".format(
         model_name=model_name, task_name=task_name, batch_size=batch_size, run_name=run_name)
-    if limit is not None:
+    if limit is not "all":
         bashCommand += " --limit " + str(limit)
 
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
